1066fe12da79b4b1e1800f96d52e2af08da7045f
Sebastian Thiel <byronimo@gmail.com>
2018-11-11 14:33:55 +0100

    probably that does it

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,27 @@
-use std::{io, io::Read};
+use std::{io, str::FromStr, io::Read};
 
 fn main() -> Result<(), Box<std::error::Error + 'static>> {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input)?;
+    let mut lines = input.split('\n');
+    match lines.next() {
+        Some(first) => {
+            let mut nums = first.split(' ').take(3).map(usize::from_str);
+            let (num_matches, w, h) = match (nums.next(), nums.next(), nums.next()) {
+                (Some(a), Some(b), Some(c)) => (a?, b?, c?),
+                _ => panic!("exhausted first line")
+            };
+            let max_size = (w.pow(2) as f64 + h.pow(2) as f64).sqrt();
+            for match_len in lines.take(num_matches).map(usize::from_str) {
+                let match_len = match_len?;
+                if match_len as f64 > max_size {
+                    println!("NE")
+                } else {
+                    println!("DA")
+                }
+            }
+        },
+        _ => panic!("exhausted")
+    }
     Ok(())
 }
